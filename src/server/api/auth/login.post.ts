1cf91d7af7d5a81fe94eee60cbac97fa46d38fa5
yesworld <a.yesworld@gmail.com>
2022-12-26 18:39:28 +0300

    chore: change response for login

--- src/server/api/auth/login.post.ts
+++ src/server/api/auth/login.post.ts
@@ -1,16 +1,31 @@
 import { H3Event } from 'h3'
 
-export default defineEventHandler(async (event: H3Event) => {
+interface IResponse {
+  code: number
+  message: string
+  data: any
+}
+
+export default defineEventHandler(async (event: H3Event): Promise<IResponse> => {
   const body = await readBody(event)
   if (body.email !== 'admin@gmail.com') {
     event.res.statusCode = 401
     event.res.statusMessage = 'Not logged in'
-    return {}
+
+    return {
+      code: event.res.statusCode,
+      message: event.res.statusMessage,
+      data: {},
+    }
   }
 
   return {
-    JWT:
-      // eslint-disable-next-line max-len
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTUxNjIzOTAyMn0.BBOywDY8s0oxuX29VTELxSUbrZKJpAj9W4ZlRNbY9L4',
+    code: 200,
+    message: 'ok',
+    data: {
+      JWT:
+        // eslint-disable-next-line max-len
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTUxNjIzOTAyMn0.BBOywDY8s0oxuX29VTELxSUbrZKJpAj9W4ZlRNbY9L4',
+    },
   }
 })
