b76c13dc52e5f3536a0e76826752933ddef3b3fd
yesworld <a.yesworld@gmail.com>
2022-12-27 14:41:16 +0300

    chore(todo): add a note for Password component

--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -15,6 +15,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('Galleria', Galleria)
   nuxtApp.vueApp.component('Button', Button)
   nuxtApp.vueApp.component('InputText', InputText)
+
+  /**
+   * TODO: add autocomplete="on" attribute for Password component
+   * @see https://github.com/primefaces/primevue/blob/master/components/password/Password.vue
+   * [DOM] Input elements should have autocomplete attributes (suggested: "current-password"):
+   * (More info: https://goo.gl/9p2vKq) input.p-inputtext.p-component.p-password-input
+   */
   nuxtApp.vueApp.component('Password', Password)
   nuxtApp.vueApp.component('Divider', Divider)
 })
